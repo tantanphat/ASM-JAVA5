989959188f44315165df4b5b2569f7a8991a17dd
Tan Phat <nguyen04tan04phat03@gmail.com>
2024-05-22 19:25:50 +0700

    myaccount...DLinh

---
+++ src/main/java/com/example/asmjava5/Constant/SessionAttr.java
@@ -0,0 +1,8 @@
+package com.example.asmjava5.Constant;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SessionAttr {
+    public static final String CURRENT_USER = "currentUser";
+}
